ca7ed46a7bd7113aae64115d7543edf0eb362f8a
b.kihoon <masinogns@gmail.com>
2017-04-20 14:38:33 +0900

    장애 해결

--- src/UserDao.java
+++ src/UserDao.java
@@ -15,38 +15,87 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
 
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
-        preparedStatement.setString(1,id);
 
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
+        try {
+            connection = dataSource.getConnection();
 
-        User user = new User();
-        user.setId(resultSet.getString("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
+            preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setString(1,id);
+            resultSet = preparedStatement.executeQuery();
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getString("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet!=null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement!=null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection!=null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         return user;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (id, name, password) VALUES (?,?,?)");
-        preparedStatement.setString(1,user.getId());
-        preparedStatement.setString(2,user.getName());
-        preparedStatement.setString(3,user.getPassword());
 
-        preparedStatement.executeUpdate();
+        try {
+            connection = dataSource.getConnection();
 
-        preparedStatement.close();
-        connection.close();
+            preparedStatement = connection.prepareStatement("insert into userinfo (id, name, password) VALUES (?,?,?)");
+            preparedStatement.setString(1,user.getId());
+            preparedStatement.setString(2,user.getName());
+            preparedStatement.setString(3,user.getPassword());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement!=null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection!=null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
